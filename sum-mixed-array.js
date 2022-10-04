bd7914bb8e4942e814f3116fcd7c9abc4f78c752
ChaPhilip <webdevphilc@gmail.com>
2022-10-04 15:40:22 -0700

    duplicateEncode

--- sum-mixed-array.js
+++ sum-mixed-array.js
@@ -13,6 +13,9 @@ function sumMix(arr) {
   
   console.log(sumMix([9, 3, '7', '3']), 22)
   console.log(sumMix(['5','0',9,3,2,1,'9',6,7]),42)
+
+
   
   
   // +c ===  Number(c)  //automatically coverts c to a number
+  
