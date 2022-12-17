2f43e523fa09d0aafe977001e2f3b6bf5cc514ff
Philip <webdevphilc@gmail.com>
2022-12-17 11:38:06 -0800

    j

---
+++ lostNumber.js
@@ -0,0 +1,14 @@
+// //Lost number in number sequence
+// // https://www.codewars.com/kata/595aa94353e43a8746000120
+// // Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+// // If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+// // All inputs will be valid.
+
+// // last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']
+// // last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']
+// // last('take me to semynak'), ['take', 'me', 'semynak', 'to'] 
+
+// Hint: someArr.indexOf(value) === -1 is a value that does not exist in someArr - throw that in a filter!  OR... think about the math...
+
